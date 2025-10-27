0725719e63c35c817a8fb851f21459b5a14f8537
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-10-27 08:07:45 -0300

    Update kodi.sh

--- kodi.sh
+++ kodi.sh
@@ -9,7 +9,7 @@ tar xf archlinux-bootstrap-x86_64.tar.zst -C ./arch/
 # criar no github uma nova pasta para o AppRun e demais arquivos.
 cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/ && cp ${GITHUB_WORKSPACE}/files/mirrorlist -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/pacman.d/ && cp ${GITHUB_WORKSPACE}/files/pacman.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/
 cd ${GITHUB_WORKSPACE}
-sudo chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Syyu --noconfirm && pacman -S kodi wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack --noconfirm && pacman -Scc --noconfirm || true && rm -rf /var/cache/pacman/pkg/*"
+sudo chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Syyu --noconfirm && pacman -S kodi wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack --noconfirm && pacman -Scc --noconfirm || true && rm -rf /var/cache/pacman/pkg/* && sed -i '18,516s/^#//' /etc/locale.gen && locale-gen"
 cp ${GITHUB_WORKSPACE}/files/AppRun ${GITHUB_WORKSPACE}/arch/ && chmod a+x ${GITHUB_WORKSPACE}/arch/AppRun && cp ${GITHUB_WORKSPACE}/files/kodi.png -t ${GITHUB_WORKSPACE}/arch/ && cp ${GITHUB_WORKSPACE}/files/kodi.desktop -t ${GITHUB_WORKSPACE}/arch/
 mv ${GITHUB_WORKSPACE}/arch/root.x86_64/  ${GITHUB_WORKSPACE}/arch/root/
 cd ./arch/
