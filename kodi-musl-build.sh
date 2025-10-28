bb906401d2c9cf474f908bca2b64c1ec75132fa3
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-10-28 08:53:00 -0300

    Update kodi-musl-build.sh

--- kodi-musl-build.sh
+++ kodi-musl-build.sh
@@ -8,6 +8,6 @@ tar xf alpine.tar.gz -C ./alp/root/
 # criar no github uma nova pasta para o AppRun e demais arquivos.
 cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/alp/root/etc/
 cd ${GITHUB_WORKSPACE}
-sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add kodi wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack --no-cache && rm -rf /var/cache/apk/*"
+sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add kodi wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack --no-cache && rm -rf /var/cache/apk/* && exit"
 cp ${GITHUB_WORKSPACE}/files/AppRun ${GITHUB_WORKSPACE}/alp/ && chmod a+x ${GITHUB_WORKSPACE}/alp/AppRun && cp ${GITHUB_WORKSPACE}/files/kodi.svg -t ${GITHUB_WORKSPACE}/alp/ && cp ${GITHUB_WORKSPACE}/files/kodi.desktop -t ${GITHUB_WORKSPACE}/alp/
 ARCH=x86_64 VERSION=clean ./appimagetool -n ./alp/
