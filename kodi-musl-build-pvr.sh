2ab920a622681f950d7a39f01791086416849b01
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-10-28 09:04:57 -0300

    Update kodi-musl-build-pvr.sh

--- kodi-musl-build-pvr.sh
+++ kodi-musl-build-pvr.sh
@@ -10,4 +10,4 @@ cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/alp/root/etc/
 cd ${GITHUB_WORKSPACE}
 sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add kodi wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack kodi-pvr-iptvsimple kodi-inputstream-ffmpegdirect kodi-inputstream-adaptive kodi-inputstream-rtmp --no-cache && rm -rf /var/cache/apk/* && exit"
 cp ${GITHUB_WORKSPACE}/files/AppRun ${GITHUB_WORKSPACE}/alp/ && chmod a+x ${GITHUB_WORKSPACE}/alp/AppRun && cp ${GITHUB_WORKSPACE}/files/kodi.svg -t ${GITHUB_WORKSPACE}/alp/ && cp ${GITHUB_WORKSPACE}/files/kodi.desktop -t ${GITHUB_WORKSPACE}/alp/
-ARCH=x86_64 VERSION=clean ./appimagetool -n ./alp/
+ARCH=x86_64 VERSION=pvr ./appimagetool -n ./alp/
