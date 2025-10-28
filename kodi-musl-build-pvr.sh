36390ef0132dee9404bdca1ce60cd0d0a69a2cba
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-10-28 09:30:23 -0300

    Update kodi-musl-build-pvr.sh

--- kodi-musl-build-pvr.sh
+++ kodi-musl-build-pvr.sh
@@ -2,12 +2,12 @@
 sudo apt-get install desktop-file-utils debootstrap schroot perl git wget curl xz-utils bubblewrap autoconf coreutils
 wget -q "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagetool && chmod a+x appimagetool
 wget -q "https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-minirootfs-3.22.2-x86_64.tar.gz" -O alpine.tar.gz
-mkdir alp
-mkdir -p ./alp/root/
-tar xf alpine.tar.gz -C ./alp/root/
+mkdir alp2
+mkdir -p ./alp2/root/
+tar xf alp2ine.tar.gz -C ./alp2/root/
 # criar no github uma nova pasta para o AppRun e demais arquivos.
-cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/alp/root/etc/
+cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/alp2/root/etc/
 cd ${GITHUB_WORKSPACE}
-sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add kodi wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack kodi-pvr-iptvsimple kodi-inputstream-ffmpegdirect kodi-inputstream-adaptive kodi-inputstream-rtmp --no-cache && rm -rf /var/cache/apk/* && exit"
-cp ${GITHUB_WORKSPACE}/files/AppRun ${GITHUB_WORKSPACE}/alp/ && chmod a+x ${GITHUB_WORKSPACE}/alp/AppRun && cp ${GITHUB_WORKSPACE}/files/kodi.svg -t ${GITHUB_WORKSPACE}/alp/ && cp ${GITHUB_WORKSPACE}/files/kodi.desktop -t ${GITHUB_WORKSPACE}/alp/
-ARCH=x86_64 VERSION=pvr ./appimagetool -n ./alp/
+sudo chroot ./alp2/root/ /bin/ash -l -c "apk update && apk upgrade && apk add kodi wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack kodi-pvr-iptvsimple kodi-inputstream-ffmpegdirect kodi-inputstream-adaptive kodi-inputstream-rtmp --no-cache && rm -rf /var/cache/apk/* && exit"
+cp ${GITHUB_WORKSPACE}/files/AppRun ${GITHUB_WORKSPACE}/alp2/ && chmod a+x ${GITHUB_WORKSPACE}/alp2/AppRun && cp ${GITHUB_WORKSPACE}/files/kodi.svg -t ${GITHUB_WORKSPACE}/alp2/ && cp ${GITHUB_WORKSPACE}/files/kodi.desktop -t ${GITHUB_WORKSPACE}/alp2/
+ARCH=x86_64 VERSION=pvr ./appimagetool -n ./alp2/
