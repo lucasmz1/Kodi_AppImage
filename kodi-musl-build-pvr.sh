27c357dd90fb25a7ee9d7aacf1bd15d08412d531
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-10-28 10:26:17 -0300

    Update kodi-musl-build-pvr.sh

--- kodi-musl-build-pvr.sh
+++ kodi-musl-build-pvr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get install desktop-file-utils debootstrap schroot perl git wget curl xz-utils bubblewrap autoconf coreutils
 wget -q "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagetool && chmod a+x appimagetool
-wget -q "https://dl-cdn.alpinelinux.org/alpine/edge/releases/x86_64/alpine-minirootfs-20251016-x86_64.tar.gz" -O alpine.tar.gz
+wget -q "https://dl-cdn.alpinelinux.org/alpine/edge/releases/x86_64/alpine-minirootfs-20251016-x86_64.tar.gz" -O alpine2.tar.gz
 mkdir alp2
 mkdir -p ./alp2/root/
-tar xf alpine.tar.gz -C ./alp2/root/
+tar xf alpine2.tar.gz -C ./alp2/root/
 # criar no github uma nova pasta para o AppRun e demais arquivos.
 cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/alp2/root/etc/
 cd ${GITHUB_WORKSPACE}
